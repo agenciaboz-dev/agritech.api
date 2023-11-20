b58fdf8720a9c951e9940adeb4d7ae939923c968
nandobfer <nandobfer@yahoo.com.br>
2023-11-20 11:54:35 -0300

    new server

--- dist/src/normalize.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const normalize = (string) => string
-    .toLocaleLowerCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "");
-exports.default = normalize;
