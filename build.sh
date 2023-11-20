f2720734cd40e014a64a34368e8d6eec84fc038d
nandobfer <nandobfer@yahoo.com.br>
2023-11-20 11:55:41 -0300

    new server

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+api="agritech.api"
+user="burgos"
+
+path="~/${api}/"
+
+echo 'compiling server filies locally'
+yarn build
+
+echo 'Uploading build to server'
+scp -r dist/* ${user}@agencyboz:${path}/dist/
+
+echo 'restarting server api'
+ssh ${user}@agencyboz "source ~/.nvm/nvm.sh; pm2 restart ${api}"
