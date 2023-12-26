a495a2eeadf33f234d5698163d7ff3450c65dcbc
tainarasn <tainarasn.sba@gmail.com>
2023-12-26 08:45:15 -0300

    fix: console in geolocal test

--- src/io/geolocalTest.ts
+++ src/io/geolocalTest.ts
@@ -7,9 +7,9 @@ const coordinate = async (socket: Socket, data: any) => {
             headers: { Authorization: `Token token=58e973c730ad8f3684584e19e68b6044` },
         })
         socket.emit("coordinate:cep:success", response.data)
-        console.log(response)
+        console.log(response.data)
     } catch (error) {
-        console.error(error)
+        // console.error(error) 
         socket.emit("coordinate:cep:error")
     }
 }
