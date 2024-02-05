9ecde81722f420675cfb5f9fe1d600d9f0231529
tainarasn <tainarasn.sba@gmail.com>
2024-02-05 08:26:37 -0300

    fix: subi

--- prisma/migrations/20240202151814_remove_unique/migration.sql
+++ prisma/migrations/20240202151814_remove_unique/migration.sql
@@ -1,24 +1,24 @@
-/*
-  Warnings:
+-- /*
+--   Warnings:
 
-  - You are about to drop the column `callId` on the `Report` table. All the data in the column will be lost.
-  - You are about to drop the column `tillageId` on the `Talhao` table. All the data in the column will be lost.
+--   - You are about to drop the column `callId` on the `Report` table. All the data in the column will be lost.
+--   - You are about to drop the column `tillageId` on the `Talhao` table. All the data in the column will be lost.
 
-*/
--- DropForeignKey
-ALTER TABLE `Report` DROP FOREIGN KEY `Report_callId_fkey`;
+-- */
+-- -- DropForeignKey
+-- ALTER TABLE `Report` DROP FOREIGN KEY `Report_callId_fkey`;
 
--- DropForeignKey
-ALTER TABLE `Talhao` DROP FOREIGN KEY `Talhao_tillageId_fkey`;
+-- -- DropForeignKey
+-- ALTER TABLE `Talhao` DROP FOREIGN KEY `Talhao_tillageId_fkey`;
 
--- AlterTable
-ALTER TABLE `Call` ADD COLUMN `reportId` INTEGER NULL;
+-- -- AlterTable
+-- ALTER TABLE `Call` ADD COLUMN `reportId` INTEGER NULL;
 
--- AlterTable
-ALTER TABLE `Report` DROP COLUMN `callId`;
+-- -- AlterTable
+-- ALTER TABLE `Report` DROP COLUMN `callId`;
 
--- AlterTable
-ALTER TABLE `Talhao` DROP COLUMN `tillageId`;
+-- -- AlterTable
+-- ALTER TABLE `Talhao` DROP COLUMN `tillageId`;
 
--- AddForeignKey
-ALTER TABLE `Call` ADD CONSTRAINT `Call_reportId_fkey` FOREIGN KEY (`reportId`) REFERENCES `Report`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- -- AddForeignKey
+-- ALTER TABLE `Call` ADD CONSTRAINT `Call_reportId_fkey` FOREIGN KEY (`reportId`) REFERENCES `Report`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20240202151937_add/migration.sql
+++ prisma/migrations/20240202151937_add/migration.sql
@@ -1,11 +1,11 @@
-/*
-  Warnings:
+-- /*
+--   Warnings:
 
-  - Added the required column `tillageId` to the `Talhao` table without a default value. This is not possible if the table is not empty.
+--   - Added the required column `tillageId` to the `Talhao` table without a default value. This is not possible if the table is not empty.
 
-*/
--- AlterTable
-ALTER TABLE `Talhao` ADD COLUMN `tillageId` INTEGER NOT NULL;
+-- */
+-- -- AlterTable
+-- ALTER TABLE `Talhao` ADD COLUMN `tillageId` INTEGER NOT NULL;
 
--- AddForeignKey
-ALTER TABLE `Talhao` ADD CONSTRAINT `Talhao_tillageId_fkey` FOREIGN KEY (`tillageId`) REFERENCES `Tillage`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- -- AddForeignKey
+-- ALTER TABLE `Talhao` ADD CONSTRAINT `Talhao_tillageId_fkey` FOREIGN KEY (`tillageId`) REFERENCES `Tillage`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
