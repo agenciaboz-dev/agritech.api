af0d698d96e971d3fc177013bc630d50de0c4c2b
astriel-git <lpatriciopro@outlook.com>
2024-02-06 13:14:32 -0300

    cleancode: tillage

--- src/databaseHandler/tillage.ts
+++ src/databaseHandler/tillage.ts
@@ -1,124 +1,131 @@
-import { OpenCall } from "../definitions/call"
-import { NewTillage } from "../definitions/tillage"
-import { PrismaClient } from "@prisma/client"
-import talhao from "./talhao"
+import { OpenCall } from "../definitions/call";
+import { NewTillage } from "../definitions/tillage";
+import { PrismaClient } from "@prisma/client";
+import talhao from "./talhao";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const inclusions = {
-    tillage: { address: true, location: true, gallery: true, call: true, talhao: { include: { calls: true } } },
-    address: { use: true, tillage: true },
-    location: { tillage: true },
-    gallery: {},
-    call: { stages: true, report: true },
-}
+  tillage: {
+    address: true,
+    location: true,
+    gallery: true,
+    call: true,
+    talhao: { include: { calls: true } },
+  },
+  address: { use: true, tillage: true },
+  location: { tillage: true },
+  gallery: {},
+  call: { stages: true, report: true },
+};
 const create = async (data: NewTillage) => {
-    console.log(data)
-    console.log("Iniciando a criação da Lavoura...")
-    const tillage = await prisma.tillage.create({
-        data: {
-            name: data.name,
-            area: Number(data.area),
-            owner: data.owner,
-            ceo: data.ceo,
-            manager: data.manager,
-            agronomist: data.agronomist,
-            technician: data.technician,
-            pilot: data.pilot,
-            others: data.others,
-            comments: data.comments,
-            producerId: data.producerId,
-        },
-    })
-    console.log({ address: data.address })
+  console.log(data);
+  const tillage = await prisma.tillage.create({
+    data: {
+      name: data.name,
+      area: Number(data.area),
+      owner: data.owner,
+      ceo: data.ceo,
+      manager: data.manager,
+      agronomist: data.agronomist,
+      technician: data.technician,
+      pilot: data.pilot,
+      others: data.others,
+      comments: data.comments,
+      producerId: data.producerId,
+    },
+  });
+  console.log({ address: data.address });
 
-    const address = await prisma.address.create({
-        data: {
-            street: data.address.street,
-            number: data.address.number,
-            city: data.address.city,
-            cep: data.address.cep,
-            adjunct: data.address.adjunct || "",
-            district: data.address.district,
-            uf: data.address.uf,
+  const address = await prisma.address.create({
+    data: {
+      street: data.address.street,
+      number: data.address.number,
+      city: data.address.city,
+      cep: data.address.cep,
+      adjunct: data.address.adjunct || "",
+      district: data.address.district,
+      uf: data.address.uf,
+      tillageId: tillage.id,
+    },
+  });
+  // Criação das coordenadas
+  if (data.location) {
+    const locations = await Promise.all(
+      data.location.map(async (location) => {
+        return await prisma.coordinate.create({
+          data: {
+            x: location.x,
+            y: location.y,
             tillageId: tillage.id,
-        },
-    })
-    // Criação das coordenadas
-    if (data.location) {
-        const locations = await Promise.all(
-            data.location.map(async (location) => {
-                return await prisma.coordinate.create({
-                    data: {
-                        x: location.x,
-                        y: location.y,
-                        tillageId: tillage.id,
-                    },
-                })
-            })
-        )
-    }
+          },
+        });
+      })
+    );
+  }
 
-    console.log("Lavoura criada:", tillage)
+  console.log(tillage);
 
-    return { tillage }
-}
+  return { tillage };
+};
 
 const update = async (data: NewTillage & { id: number }) => {
-    const tillage = await prisma.tillage.update({
-        where: { id: data.id },
-        data: {
-            name: data.name,
-            area: data.area,
-            owner: data.owner,
-            ceo: data.ceo,
-            manager: data.manager,
-            agronomist: data.agronomist,
-            technician: data.technician,
-            pilot: data.pilot,
-            others: data.others,
-            comments: data.comments,
-            producerId: data.producerId,
-            address: {
-                update: {
-                    street: data.address.street,
-                    number: data.address.number,
-                    city: data.address.city,
-                    cep: data.address.cep,
-                    adjunct: data.address.adjunct || "",
-                    district: data.address.district,
-                    uf: data.address.uf,
-                },
-            },
+  const tillage = await prisma.tillage.update({
+    where: { id: data.id },
+    data: {
+      name: data.name,
+      area: data.area,
+      owner: data.owner,
+      ceo: data.ceo,
+      manager: data.manager,
+      agronomist: data.agronomist,
+      technician: data.technician,
+      pilot: data.pilot,
+      others: data.others,
+      comments: data.comments,
+      producerId: data.producerId,
+      address: {
+        update: {
+          street: data.address.street,
+          number: data.address.number,
+          city: data.address.city,
+          cep: data.address.cep,
+          adjunct: data.address.adjunct || "",
+          district: data.address.district,
+          uf: data.address.uf,
         },
-        include: { address: true, producer: true },
-    })
-    console.log("tillage update: ", data)
+      },
+    },
+    include: { address: true, producer: true },
+  });
+  console.log(tillage);
 
-    return tillage
-}
+  return tillage;
+};
 
 const list = async () => {
-    return await prisma.tillage.findMany({
+  return await prisma.tillage.findMany({
+    include: {
+      address: true,
+      location: true,
+      gallery: true,
+      producer: true,
+      talhao: {
         include: {
-            address: true,
-            location: true,
-            gallery: true,
-            producer: true,
-            talhao: {
-                include: {
-                    gallery: true,
-                    location: true,
-                    tillage: true,
-                    calls: { include: { reports: true, stages: true, kit: true, producer: true } },
-                },
-            },
+          gallery: true,
+          location: true,
+          tillage: true,
+          calls: {
+            include: { reports: true, stages: true, kit: true, producer: true },
+          },
         },
-    })
-}
+      },
+    },
+  });
+};
 
 export default {
-    create,
-    update,
-    list,
-}
+  create,
+  update,
+  list,
+};
--- src/io/tillage.ts
+++ src/io/tillage.ts
@@ -2,17 +2,12 @@ import { Socket } from "socket.io";
 import databaseHandler from "../databaseHandler/tillage";
 
 const newTillage = async (socket: Socket, data: any) => {
-  console.log("Lavoura recebida:", data);
+  console.log(data);
 
   try {
     const tillage = await databaseHandler.create(data);
 
-    if (tillage) {
-      socket.emit("tillage:creation:success", tillage);
-      // socket.broadcast.emit("user:update", user)
-    } else {
-      socket.emit("tillage:creation:failed");
-    }
+    socket.emit("tillage:creation:success", tillage);
   } catch (error) {
     console.log(error);
     socket.emit("tillage:creation:failed", { error: error });
@@ -20,17 +15,11 @@ const newTillage = async (socket: Socket, data: any) => {
 };
 
 const updateTillage = async (socket: Socket, data: any) => {
-  console.log("Lavoura recebida atualizada:", data);
+  console.log(data);
 
   try {
     const tillage = await databaseHandler.update(data);
-
-    if (tillage) {
-      socket.emit("tillage:update:success", tillage);
-      // socket.broadcast.emit("user:update", user)
-    } else {
-      socket.emit("tillage:update:failed");
-    }
+    socket.emit("tillage:update:success", tillage);
   } catch (error) {
     console.log(error);
     socket.emit("tillage:update:failed", { error: error });
@@ -38,7 +27,6 @@ const updateTillage = async (socket: Socket, data: any) => {
 };
 
 const listTillage = async (socket: Socket) => {
-  // console.log("Lista de tillages")
   const tillage = await databaseHandler.list();
   socket.emit("tillage:list:success", tillage);
 };
