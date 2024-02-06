99423c2f04acff0982da96d9986531036dd24723
astriel-git <lpatriciopro@outlook.com>
2024-02-06 13:13:48 -0300

    cleancode:stage

--- src/io/stage.ts
+++ src/io/stage.ts
@@ -1,80 +1,61 @@
-import { Socket } from "socket.io"
-import databaseHandler from "../databaseHandler/stage"
-import { NewStage } from "../definitions/stage"
-import { Stage } from "@prisma/client"
-import call from "./call"
+import { Socket } from "socket.io";
+import databaseHandler from "../databaseHandler/stage";
+import { NewStage } from "../definitions/stage";
+import { Stage } from "@prisma/client";
+import call from "./call";
 
-// Cria o Stage 1
 const newStage = async (socket: Socket, data: NewStage) => {
-    console.log("New Stage:", data)
+  console.log("Creating STAGE1:", data);
 
-    try {
-        const stage = await databaseHandler.create(data)
+  try {
+    const stage = await databaseHandler.create(data);
+    socket.emit("stage:creation:success", stage);
+  } catch (error) {
+    console.log(error);
+    socket.emit("stage:creation:failed", { error: error });
+  }
+};
 
-        if (stage) {
-            socket.emit("stage:creation:success", stage)
-        } else {
-            socket.emit("stage:creation:failed")
-        }
-    } catch (error) {
-        console.log(error)
-        socket.emit("stage:update:failed", { error: error })
-    }
-}
-
-// Atualiza o Stage 1 e Cria o Stage 2
+// Atualiza o Stage 1 para 2 e Cria o Stage 3
 const updateStageOne = async (socket: Socket, data: Stage) => {
-    try {
-        const stage = await databaseHandler.updateOne(data)
+  try {
+    const stage = await databaseHandler.updateOne(data);
+    socket.emit("stage:updateOne:success", stage.stage1);
+    socket.emit("stage:creationOne:success", stage.stage2);
+    socket.emit("call:updateOne:success", stage.updatedCall);
+  } catch (error) {
+    console.log(error);
+    socket.emit("stage:updateOne:failed", { error: error });
+  }
+};
 
-        if (stage) {
-            socket.emit("stage:updateOne:success", stage.stage1)
-            socket.emit("stage:creationOne:success", stage.stage2)
-            socket.emit("call:updateOne:success", stage.updatedCall)
-        } else {
-            socket.emit("stage:updateOne:failed")
-        }
-    } catch (error) {
-        console.log(error)
-        socket.emit("stage:updateOne:failed", { error: error })
-    }
-}
 // Atualiza o Stage 2 e Cria o Stage 3
 const updateStageTwo = async (socket: Socket, data: Stage) => {
-    try {
-        const stage = await databaseHandler.updateTwo(data)
-
-        if (stage) {
-            socket.emit("stage:updateTwo:success", stage.stage2)
-            socket.emit("stage:creationTwo:success", stage.stage3)
-            socket.emit("call:updateTwo:success", stage.updatedCall)
-        } else {
-            socket.emit("stage:updateTwo:failed")
-        }
-    } catch (error) {
-        console.log(error)
-        socket.emit("stage:updateTwo:failed", { error: error })
-    }
-}
+  try {
+    const stage = await databaseHandler.updateTwo(data);
+    socket.emit("stage:updateTwo:success", stage.stage2);
+    socket.emit("stage:creationTwo:success", stage.stage3);
+    socket.emit("call:updateTwo:success", stage.updatedCall);
+  } catch (error) {
+    console.log(error);
+    socket.emit("stage:updateTwo:failed", { error: error });
+  }
+};
+// Atualiza o Stage 3 para 4
 const updateStageThree = async (socket: Socket, data: Stage) => {
-    try {
-        const stage = await databaseHandler.updateThree(data)
-
-        if (stage) {
-            socket.emit("stage:updateThree:success", stage.stage3)
-            socket.emit("call:updateThree:success", stage.updatedCall)
-        } else {
-            socket.emit("stage:updateThree:failed")
-        }
-    } catch (error) {
-        console.log(error)
-        socket.emit("stage:updateThree:failed", { error: error })
-    }
-}
+  try {
+    const stage = await databaseHandler.updateThree(data);
+    socket.emit("stage:updateThree:success", stage.stage3);
+    socket.emit("call:updateThree:success", stage.updatedCall);
+  } catch (error) {
+    console.log(error);
+    socket.emit("stage:updateThree:failed", { error: error });
+  }
+};
 
 export default {
-    newStage,
-    updateStageOne,
-    updateStageTwo,
-    updateStageThree,
-}
+  newStage,
+  updateStageOne,
+  updateStageTwo,
+  updateStageThree,
+};
