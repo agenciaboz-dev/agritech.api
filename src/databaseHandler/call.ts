ff4498cc07e7e87cdbc4f7d51d34568c5177d665
astriel-git <lpatriciopro@outlook.com>
2024-02-14 11:33:02 -0300

    Updates: Fixing Report and Call pipeline

--- src/databaseHandler/call.ts
+++ src/databaseHandler/call.ts
@@ -1,341 +1,347 @@
-import { Call, PrismaClient } from "@prisma/client"
-import { OpenCall, AdminCall } from "../definitions/call"
+import { Call, PrismaClient } from "@prisma/client";
+import { OpenCall, AdminCall, ApproveCall } from "../definitions/call";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const inclusions = {
-    call: {
-        stages: true,
-        tillage: {
-            include: {
-                address: true,
-                location: true,
-                gallery: true,
-                talhao: { include: { name: true, area: true, location: true } },
-            },
-        },
-        report: true,
-        kit: true,
-        producer: {
-            include: { tillage: true, user: true },
-        },
-        user: true,
+  call: {
+    tillage: {
+      include: {
+        address: true,
+        location: true,
+        gallery: true,
+        talhao: { include: { name: true, area: true, location: true } },
+      },
     },
-}
+    report: true,
+    kit: true,
+    producer: {
+      include: { tillage: true, user: true },
+    },
+    user: true,
+  },
+};
 
 const adminCreate = async (data: AdminCall) => {
-    try {
-        console.log({ Ola_aqui: data })
+  try {
+    console.log({ Ola_aqui: data });
 
-        // Create the call
-        const call = await prisma.call.create({
-            data: {
-                open: new Date().getTime().toString(),
-                approved: data.approved,
-                comments: data.comments,
-                talhaoId: data.talhaoId,
-                producerId: data.producerId,
-                userId: data.userId,
-                kitId: data.kitId || undefined,
-                forecast: data.forecast,
-            },
-        })
+    // Create the call
+    const call = await prisma.call.create({
+      data: {
+        open: new Date().getTime().toString(),
+        approved: data.approved,
+        comments: data.comments,
+        talhaoId: data.talhaoId,
+        producerId: data.producerId,
+        userId: data.userId,
+        kitId: data.kitId || undefined,
+        forecast: data.forecast,
+      },
+    });
 
-        // Update the producer
-        const findTalhao = await prisma.talhao.findUnique({ where: { id: data.talhaoId } })
-        const findTillage = await prisma.tillage.findUnique({ where: { id: findTalhao?.tillageId } })
-        console.log({ talhao: findTalhao })
-        const tillage = await prisma.tillage.update({
-            where: { id: findTalhao?.tillageId },
-            data: {
-                hectarePrice: data.hectarePrice || findTillage?.hectarePrice,
-            },
-        })
+    // Update the producer
+    const findTalhao = await prisma.talhao.findUnique({
+      where: { id: data.talhaoId },
+    });
+    const findTillage = await prisma.tillage.findUnique({
+      where: { id: findTalhao?.tillageId },
+    });
+    console.log({ talhao: findTalhao });
+    const tillage = await prisma.tillage.update({
+      where: { id: findTalhao?.tillageId },
+      data: {
+        hectarePrice: data.hectarePrice || findTillage?.hectarePrice,
+      },
+    });
 
-        // Create the stage
-        const stage = await prisma.stage.create({
-            data: {
-                name: "STAGE1",
-                callId: call.id,
-            },
-        })
+    // Create the stage
+    const stage = await prisma.stage.create({
+      data: {
+        name: "STAGE1",
+        reportId: call.id,
+      },
+    });
 
-        // Update the call
-        const updatedCall = await prisma.call.update({
-            where: { id: call.id },
-            data: {
-                approved: data.kitId ? true : false,
-                status: "INPROGRESS",
-                stage: "STAGE1",
-                kitId: data.kitId,
-                init: new Date().getTime().toString(),
-            },
-        })
+    // Update the call
+    const updatedCall = await prisma.call.update({
+      where: { id: call.id },
+      data: {
+        approved: data.kitId ? true : false,
+        status: "INPROGRESS",
+        kitId: data.kitId,
+        init: new Date().getTime().toString(),
+      },
+    });
 
-        console.log("Call, Producer, Stage, and Updated Call created/updated:", {
-            call,
-            tillage,
-            stage,
-            updatedCall,
-        })
+    console.log("Call, Producer, Stage, and Updated Call created/updated:", {
+      call,
+      tillage,
+      stage,
+      updatedCall,
+    });
 
-        return { call, tillage, stage, updatedCall }
-    } catch (error) {
-        console.error("Error in creating and updating the call:", error)
-        throw error
-    }
-}
+    return { call, tillage, stage, updatedCall };
+  } catch (error) {
+    console.error("Error in creating and updating the call:", error);
+    throw error;
+  }
+};
 
 const create = async (data: OpenCall) => {
-    console.log(data)
-    const call = await prisma.call.create({
-        data: {
-            open: new Date().getTime().toString(),
-            approved: data.approved,
-            comments: data.comments,
-            talhaoId: data.talhaoId,
-            producerId: data.producerId,
-            userId: data.userId,
-            kitId: data.kitId || undefined,
-            forecast: data.forecast,
-        },
-    })
+  console.log(data);
+  const call = await prisma.call.create({
+    data: {
+      open: new Date().getTime().toString(),
+      approved: data.approved,
+      comments: data.comments,
+      talhaoId: data.talhaoId,
+      producerId: data.producerId,
+      userId: data.userId,
+      kitId: data.kitId || undefined,
+      forecast: data.forecast,
+    },
+  });
 
-    const tillage = await prisma.tillage.update({
-        where: { id: data.talhao.tillageId },
-        data: {
-            hectarePrice: data.hectarePrice,
-        },
-    })
+  const tillage = await prisma.tillage.update({
+    where: { id: data.talhao.tillageId },
+    data: {
+      hectarePrice: data.hectarePrice,
+    },
+  });
 
-    console.log({ call, tillage })
-    return { call, tillage }
-}
+  console.log({ call, tillage });
+  return { call, tillage };
+};
 
 const update = async (data: Call) => {
-    console.log(data)
-    const call = await prisma.call.update({
+  console.log(data);
+  const call = await prisma.call.update({
+    where: { id: data.id },
+    data: {
+      open: data.open,
+      init: data.init,
+      finish: data.finish,
+      approved: data.approved,
+      comments: data.comments,
+      status: data.status,
+      talhaoId: data.talhaoId,
+      producerId: data.producerId,
+      userId: data.userId,
+      kitId: data.kitId,
+      totalPrice: data.totalPrice,
+    },
+  });
+  console.log({ call });
+  return call;
+};
+
+const approve = async (data: ApproveCall) => {
+  try {
+    const call = await prisma.call.findUnique({ where: { id: data.id } });
+
+    if (call) {
+      const existingReport = await prisma.report.findFirst({
+        where: {
+          callId: data.id,
+          stage: "STAGE1",
+        },
+      });
+
+      if (existingReport) {
+        throw new Error(
+          "A STAGE1 status stage already exists for this call's initial report"
+        );
+      }
+
+      const updatedCall = await prisma.call.update({
         where: { id: data.id },
         data: {
-            open: data.open,
-            init: data.init,
-            finish: data.finish,
-            approved: data.approved,
-            comments: data.comments,
-            status: data.status,
-            stage: data.stage,
-            talhaoId: data.talhaoId,
-            producerId: data.producerId,
-            userId: data.userId,
-            kitId: data.kitId,
-            totalPrice: data.totalPrice,
+          approved: data.kitId ? true : false,
+          status: "INPROGRESS",
+          kitId: data.kitId,
+          init: new Date().getTime().toString(),
         },
-    })
-    console.log({ call })
-    return call
-}
+      });
 
-const approve = async (data: OpenCall) => {
-    try {
-        const call = await prisma.call.findUnique({ where: { id: data.id } })
+      const tillage = await prisma.tillage.update({
+        where: { id: data.talhao.tillageId },
+        data: {
+          hectarePrice: data.hectarePrice ?? undefined,
+        },
+      });
 
-        if (call) {
-            const existingStage = await prisma.stage.findFirst({
-                where: {
-                    callId: data.id,
-                    name: "STAGE1",
-                },
-            })
+      const report = await prisma.report.create({
+        data: {
+          stage: "STAGE1",
+          callId: updatedCall.id,
+        },
+      });
 
-            if (existingStage) {
-                throw new Error("A STAGE1 status stage already exists for this call")
-            }
+      const stage = await prisma.stage.create({
+        data: {
+          name: "STAGE1",
+          reportId: report.id,
+        },
+      });
+      console.log(stage);
 
-            const updatedCall = await prisma.call.update({
-                where: { id: data.id },
-                data: {
-                    approved: data.kitId ? true : false,
-                    status: "INPROGRESS",
-                    stage: "STAGE1",
-                    kitId: data.kitId,
-                    init: new Date().getTime().toString(),
-                },
-            })
-
-            const tillage = await prisma.tillage.update({
-                where: { id: data.talhao.tillageId },
-                data: {
-                    hectarePrice: data.hectarePrice ?? undefined,
-                },
-            })
-
-            const stage = await prisma.stage.create({
-                data: {
-                    name: "STAGE1",
-                    callId: updatedCall.id,
-                },
-            })
-            console.log(stage)
-
-            return { call: updatedCall, stage, tillage }
-        } else {
-            throw new Error("Call not found or kit already assigned")
-        }
-    } catch (error) {
-        console.error("Error in approving call:", error)
-        throw error
+      return { call: updatedCall, stage, tillage };
+    } else {
+      throw new Error("Call not found or kit already assigned");
     }
-}
+  } catch (error) {
+    console.error("Error in approving call:", error);
+    throw error;
+  }
+};
 
 const close = async (data: Call) => {
-    console.log(data)
-    const call = await prisma.call.update({
-        where: { id: data.id },
-        data: {
-            finish: new Date().getTime().toString(),
-            status: "CLOSED",
-        },
-    })
-    console.log({ call })
-    return { call }
-}
+  console.log(data);
+  const call = await prisma.call.update({
+    where: { id: data.id },
+    data: {
+      finish: new Date().getTime().toString(),
+      status: "CLOSED",
+    },
+  });
+  console.log({ call });
+  return { call };
+};
 
 const cancel = async (data: Call) => {
-    console.log(data)
-    const call = await prisma.call.update({
-        where: { id: data.id },
-        data: {
-            status: "CANCELED",
-        },
-    })
-    console.log({ call })
+  console.log(data);
+  const call = await prisma.call.update({
+    where: { id: data.id },
+    data: {
+      status: "CANCELED",
+    },
+  });
+  console.log({ call });
 
-    const report = await prisma.report.create({
-        data: {
-            callId: call.id,
-        },
-    })
-    console.log("Report criado para o chamado:", report)
-    return { call, report }
-}
+  // const report = await prisma.report.create({
+  //   data: {
+  //     callId: call.id,
+  //   },
+  // });
+  // console.log("Report criado para o chamado:", report);
+  return { call };
+};
 
 const list = async () => {
-    const calls = await prisma.call.findMany({
-        include: {
-            kit: { include: { employees: true, calls: true, objects: true } },
-            producer: { include: { user: true } },
-            user: true,
-            stages: true,
-            talhao: true,
+  const call = await prisma.call.findMany({
+    include: {
+      kit: { include: { employees: true, calls: true, objects: true } },
+      producer: { include: { user: true } },
+      user: true,
+      talhao: true,
 
-            reports: {
-                include: {
-                    operation: true,
-                },
-            },
-        },
-    })
+      reports: true,
+    },
+  });
 
-    // Process each call and update the totalPrice
-    const updatedCalls = await Promise.all(
-        calls.map(async (call) => {
-            const findTillage = await prisma.tillage.findUnique({ where: { id: call.talhao?.tillageId || 0 } })
-            const tillageHectarePrice = findTillage?.hectarePrice || 0
+  // // Process each call and update the totalPrice
+  // const updatedCalls = await Promise.all(
+  //   calls.map(async (call) => {
+  //     const findTillage = await prisma.tillage.findUnique({
+  //       where: { id: call.talhao?.tillageId || 0 },
+  //     });
+  //     const tillageHectarePrice = findTillage?.hectarePrice || 0;
 
-            // Calculate the total areaTrabalhada from all reports
-            const totalAreaTrabalhada = call.reports.reduce((total, report) => {
-                return total + (report.areaTrabalhada || 0)
-            }, 0)
+  //     // Calculate the total areaTrabalhada from all reports
+  //     const totalAreaTrabalhada = call.reports.reduce((total, report) => {
+  //       return total + (report.areaTrabalhada || 0);
+  //     }, 0);
 
-            // Calculate the desired value for each call
-            const calculatedValue = tillageHectarePrice * totalAreaTrabalhada
+  //     // Calculate the desired value for each call
+  //     const calculatedValue = tillageHectarePrice * totalAreaTrabalhada;
 
-            // Update the totalPrice in the database
-            const updatedCall = await prisma.call.update({
-                where: { id: call.id },
-                data: {
-                    totalPrice: calculatedValue,
-                },
-                include: {
-                    kit: { include: { employees: true, calls: true, objects: true } },
-                    producer: { include: { user: true } },
-                    user: true,
-                    stages: true,
-                    talhao: true,
-                    reports: {
-                        include: {
-                            operation: true,
-                        },
-                    },
-                },
-            })
+  //     // Update the totalPrice in the database
+  //     const updatedCall = await prisma.call.update({
+  //       where: { id: call.id },
+  //       data: {
+  //         totalPrice: calculatedValue,
+  //       },
+  //       include: {
+  //         kit: { include: { employees: true, calls: true, objects: true } },
+  //         producer: { include: { user: true } },
+  //         user: true,
+  //         talhao: true,
+  //         reports: {
+  //           include: {
+  //             operation: true,
+  //           },
+  //         },
+  //       },
+  //     });
 
-            return updatedCall
-        })
-    )
+  //     return updatedCall;
+  //   })
+  // );
 
-    return updatedCalls
-}
+  return call;
+};
 
 const listPending = async () => {
-    return await prisma.call.findMany({
-        where: { approved: false },
+  return await prisma.call.findMany({
+    where: { approved: false },
+    include: {
+      kit: true,
+      producer: { include: { user: true } },
+      user: true,
+      talhao: {
         include: {
-            kit: true,
-            producer: { include: { user: true } },
-            user: true,
-            talhao: {
-                include: {
-                    location: true,
-                    gallery: true,
-                    tillage: { include: { address: true, gallery: true, location: true } },
-                },
-            },
+          location: true,
+          gallery: true,
+          tillage: {
+            include: { address: true, gallery: true, location: true },
+          },
         },
-    })
-}
+      },
+    },
+  });
+};
 const listApproved = async () => {
-    return await prisma.call.findMany({
-        where: { approved: true },
+  return await prisma.call.findMany({
+    where: { approved: true },
+    include: {
+      kit: {
         include: {
-            kit: {
-                include: {
-                    employees: { include: { user: true } },
-                    calls: true,
-                    objects: true,
-                },
-            },
-            talhao: { include: { location: true } },
-            producer: { include: { user: true } },
-            user: true,
-            stages: true,
-            reports: {
-                include: {
-                    operation: true,
-                    treatment: { include: { products: true } },
-                    material: true,
-                    techReport: { include: { flight: true } },
-                },
-            },
+          employees: { include: { user: true } },
+          calls: true,
+          objects: true,
         },
-    })
-}
+      },
+      talhao: { include: { location: true } },
+      producer: { include: { user: true } },
+      user: true,
+      reports: {
+        include: {
+          operation: true,
+          treatment: { include: { products: true } },
+          material: true,
+          techReport: { include: { flight: true } },
+        },
+      },
+    },
+  });
+};
 
-const find = async (id: number) => {
-    const report = await prisma.report.findUnique({ where: { id } })
-    console.log({ report })
-    return report
-}
+// const find = async (id: number) => {
+//   const report = await prisma.report.findUnique({ where: { id } });
+//   console.log({ report });
+//   return report;
+// };
 
 export default {
-    adminCreate,
-    create,
-    update,
-    approve,
-    close,
-    cancel,
-    list,
-    listPending,
-    listApproved,
-    find,
-}
+  adminCreate,
+  create,
+  update,
+  approve,
+  close,
+  cancel,
+  list,
+  listPending,
+  listApproved,
+  // find,
+};
--- src/databaseHandler/tillage.ts
+++ src/databaseHandler/tillage.ts
@@ -1,133 +1,133 @@
-import { OpenCall } from "../definitions/call"
-import { NewTillage } from "../definitions/tillage"
-import { PrismaClient } from "@prisma/client"
-import talhao from "./talhao"
+import { OpenCall } from "../definitions/call";
+import { NewTillage } from "../definitions/tillage";
+import { PrismaClient } from "@prisma/client";
+import talhao from "./talhao";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const inclusions = {
-    tillage: {
-        address: true,
-        location: true,
-        gallery: true,
-        call: true,
-        talhao: { include: { calls: true } },
-    },
-    address: { use: true, tillage: true },
-    location: { tillage: true },
-    gallery: {},
-    call: { stages: true, report: true },
-}
+  tillage: {
+    address: true,
+    location: true,
+    gallery: true,
+    call: true,
+    talhao: { include: { calls: true } },
+  },
+  address: { use: true, tillage: true },
+  location: { tillage: true },
+  gallery: {},
+  call: { stages: true, report: true },
+};
 const create = async (data: NewTillage) => {
-    console.log(data)
-    const tillage = await prisma.tillage.create({
-        data: {
-            name: data.name,
-            area: Number(data.area),
-            owner: data.owner,
-            ceo: data.ceo,
-            manager: data.manager,
-            agronomist: data.agronomist,
-            technician: data.technician,
-            pilot: data.pilot,
-            others: data.others,
-            comments: data.comments,
-            producerId: data.producerId,
-            hectarePrice: data.hectarePrice || 0,
-        },
-    })
-    console.log({ address: data.address })
+  console.log(data);
+  const tillage = await prisma.tillage.create({
+    data: {
+      name: data.name,
+      area: Number(data.area),
+      owner: data.owner,
+      ceo: data.ceo,
+      manager: data.manager,
+      agronomist: data.agronomist,
+      technician: data.technician,
+      pilot: data.pilot,
+      others: data.others,
+      comments: data.comments,
+      producerId: data.producerId,
+      hectarePrice: data.hectarePrice || 0,
+    },
+  });
+  console.log({ address: data.address });
 
-    const address = await prisma.address.create({
-        data: {
-            street: data.address.street,
-            number: data.address.number,
-            city: data.address.city,
-            cep: data.address.cep,
-            adjunct: data.address.adjunct || "",
-            district: data.address.district,
-            uf: data.address.uf,
+  const address = await prisma.address.create({
+    data: {
+      street: data.address.street,
+      number: data.address.number,
+      city: data.address.city,
+      cep: data.address.cep,
+      adjunct: data.address.adjunct || "",
+      district: data.address.district,
+      uf: data.address.uf,
+      tillageId: tillage.id,
+    },
+  });
+  // Criação das coordenadas
+  if (data.location) {
+    const locations = await Promise.all(
+      data.location.map(async (location) => {
+        return await prisma.coordinate.create({
+          data: {
+            x: location.x,
+            y: location.y,
             tillageId: tillage.id,
-        },
-    })
-    // Criação das coordenadas
-    if (data.location) {
-        const locations = await Promise.all(
-            data.location.map(async (location) => {
-                return await prisma.coordinate.create({
-                    data: {
-                        x: location.x,
-                        y: location.y,
-                        tillageId: tillage.id,
-                    },
-                })
-            })
-        )
-    }
+          },
+        });
+      })
+    );
+  }
 
-    console.log(tillage)
+  console.log(tillage);
 
-    return { tillage }
-}
+  return { tillage };
+};
 
 const update = async (data: NewTillage & { id: number }) => {
-    const tillage = await prisma.tillage.update({
-        where: { id: data.id },
-        data: {
-            name: data.name,
-            area: data.area,
-            owner: data.owner,
-            ceo: data.ceo,
-            manager: data.manager,
-            agronomist: data.agronomist,
-            technician: data.technician,
-            pilot: data.pilot,
-            others: data.others,
-            comments: data.comments,
-            producerId: data.producerId,
-            hectarePrice: data.hectarePrice,
-            address: {
-                update: {
-                    street: data.address.street,
-                    number: data.address.number,
-                    city: data.address.city,
-                    cep: data.address.cep,
-                    adjunct: data.address.adjunct || "",
-                    district: data.address.district,
-                    uf: data.address.uf,
-                },
-            },
+  const tillage = await prisma.tillage.update({
+    where: { id: data.id },
+    data: {
+      name: data.name,
+      area: data.area,
+      owner: data.owner,
+      ceo: data.ceo,
+      manager: data.manager,
+      agronomist: data.agronomist,
+      technician: data.technician,
+      pilot: data.pilot,
+      others: data.others,
+      comments: data.comments,
+      producerId: data.producerId,
+      hectarePrice: data.hectarePrice,
+      address: {
+        update: {
+          street: data.address.street,
+          number: data.address.number,
+          city: data.address.city,
+          cep: data.address.cep,
+          adjunct: data.address.adjunct || "",
+          district: data.address.district,
+          uf: data.address.uf,
         },
-        include: { address: true, producer: true },
-    })
-    console.log(tillage)
+      },
+    },
+    include: { address: true, producer: true },
+  });
+  console.log(tillage);
 
-    return tillage
-}
+  return tillage;
+};
 
 const list = async () => {
-    return await prisma.tillage.findMany({
+  return await prisma.tillage.findMany({
+    include: {
+      address: true,
+      location: true,
+      gallery: true,
+      producer: true,
+      talhao: {
         include: {
-            address: true,
-            location: true,
-            gallery: true,
-            producer: true,
-            talhao: {
-                include: {
-                    gallery: true,
-                    location: true,
-                    tillage: true,
-                    calls: {
-                        include: { reports: true, stages: true, kit: true, producer: true },
-                    },
-                },
-            },
+          gallery: true,
+          location: true,
+          tillage: true,
+          calls: {
+            include: { reports: true, kit: true, producer: true },
+          },
         },
-    })
-}
+      },
+    },
+  });
+};
 
 export default {
-    create,
-    update,
-    list,
-}
+  create,
+  update,
+  list,
+};
