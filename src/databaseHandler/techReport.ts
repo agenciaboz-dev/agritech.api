dea0a66745b0a136e5aa0e450bbef6628f903207
tainarasn <tainarasn.sba@gmail.com>
2024-02-16 14:28:12 -0300

    fix: create flight and products in tratment and techreport

--- src/databaseHandler/techReport.ts
+++ src/databaseHandler/techReport.ts
@@ -1,51 +1,64 @@
-import { Stage, PrismaClient, Report, Call } from "@prisma/client";
-import { NewTechReport } from "../definitions/techReport";
+import { Stage, PrismaClient, Report, Call } from "@prisma/client"
+import { NewTechReport } from "../definitions/techReport"
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 const create = async (data: NewTechReport) => {
-  console.log(data);
-  const techReport = await prisma.techReport.create({
-    data: {
-      date: data.date,
-      init: data.init,
-      finish: data.finish,
-      comments: data.comments,
-      reportId: data.reportId,
-    },
-  });
-  console.log({ techReport });
-  return techReport;
-};
+    console.log(data)
+    const techReport = await prisma.techReport.create({
+        data: {
+            date: data.date,
+            init: data.init,
+            finish: data.finish,
+            comments: data.comments,
+            reportId: data.reportId,
+        },
+    })
+    console.log({ techReport })
+    return techReport
+}
 
 const update = async (data: NewTechReport) => {
-  console.log(data);
-  const techReport = await prisma.techReport.update({
-    where: { id: data.id },
-    data: {
-      date: data.date,
-      init: data.init,
-      finish: data.finish,
-      comments: data.comments,
-      reportId: data.reportId,
-    },
-  });
-  console.log(techReport);
+    console.log(data)
+    const techReport = await prisma.techReport.update({
+        where: { id: data.id },
+        data: {
+            date: data.date,
+            init: data.init,
+            finish: data.finish,
+            comments: data.comments,
+            reportId: data.reportId,
+            flight: {
+                create: data.flights.map((flight) => ({
+                    humidity: flight.humidity,
+                    temperature: flight.temperature,
+                    wind_velocity: flight.wind_velocity,
+                    height: flight.height,
+                    faixa: flight.faixa,
+                    flight_velocity: flight.flight_velocity,
+                    tank_volume: flight.tank_volume,
+                    rate: flight.rate,
+                    performance: flight.performance,
+                })),
+            },
+        },
+    })
+    console.log(techReport)
 
-  return techReport;
-};
+    return techReport
+}
 
 const find = async (id: number) => {
-  return await prisma.techReport.findUnique({ where: { id } });
-};
+    return await prisma.techReport.findUnique({ where: { id } })
+}
 
 const list = async () => {
-  return await prisma.techReport.findMany({});
-};
+    return await prisma.techReport.findMany({})
+}
 
 export default {
-  create,
-  update,
-  find,
-  list,
-};
+    create,
+    update,
+    find,
+    list,
+}
